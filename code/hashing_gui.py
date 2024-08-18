518e4877cde500439adc4be89e3dfa108dd69e8d
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-18 17:38:17 +0800

    Update hashing_gui.py

--- code/hashing_gui.py
+++ code/hashing_gui.py
@@ -1,8 +1,23 @@
 from tkinter import *
 import bcrypt
 
-root = Tk()
+def validate(password):
+    hash = b'$2b$12$o2hSi17XXdYi.4e9tupIhutibgg.C6BzJUrmdWOGJu.MD8IAqvjlK'
+    entered_password = bytes(password, encoding='utf-8')
+    if bcrypt.checkpw(entered_password, hash):
+        print('Login successful!')
+    else:
+        print('Invalid password!')
 
+root = Tk()
+root.geometry("300x300")
+
+password_entry = Entry(root)
+password_entry.pack()
+
+button = Button(text="validate",
+                command=lambda: validate(password_entry.get()))
+button.pack()
 
 
 root.mainloop()
